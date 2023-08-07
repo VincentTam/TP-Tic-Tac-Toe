b15c64cffdbc7faeeb2e10424c13f0ed4ded1067
Vincent Tam <sere@live.hk>
2023-08-07 17:07:04 +0200

    mon essai jq FIN du J

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,24 +4,13 @@ import Square from "./Square";
 
 // needs root element
 export default function Board() {
-  const changeSquareValue = (e) => {e.target.value = e.target.value === "O" ? "X" : "O";}
-  return (
-    <>
-      <div>
-        <Square label="O" whenClicked={(e) => changeSquareValue(e)}/>
-        <Square label="O" whenClicked={(e) => changeSquareValue(e)}/>
-        <Square label="O" whenClicked={(e) => changeSquareValue(e)}/>
-      </div>
-      <div>
-        <Square label="O" whenClicked={(e) => changeSquareValue(e)}/>
-        <Square label="O" whenClicked={(e) => changeSquareValue(e)}/>
-        <Square label="O" whenClicked={(e) => changeSquareValue(e)}/>
-      </div>
-      <div>
-        <Square label="O" whenClicked={(e) => changeSquareValue(e)}/>
-        <Square label="O" whenClicked={(e) => changeSquareValue(e)}/>
-        <Square label="O" whenClicked={(e) => changeSquareValue(e)}/>
-      </div>
-    </>
-    );
+  const changeSquareValue = (e) => {e.target.value = e.target.value === "O" ? "X" : "O"};
+  const squareProps = {label: "O", whenClicked: (e) => changeSquareValue(e)};
+  const squares = [];
+  [...Array(3).keys()].forEach(_ => {
+    [...Array(3).keys()].forEach(_ => {
+      squares.push(Square(squareProps));
+    });
+  });
+  return (<>{squares}</>);
 }
