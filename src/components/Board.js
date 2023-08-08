91c138dceaafcacc5aa8dfada12a1d6652270275
Vincent Tam <sere@live.hk>
2023-08-08 12:36:39 +0200

    ReactJS j2b

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Square from "./Square";
 
 // needs root element
 export default function Board() {
-  const changeSquareValue = (e) => {e.target.value = e.target.value === "O" ? "X" : "O"};
-  const squares = [...Array(9).keys()].map(_ => (
-    <Square label="O" whenClicked={(e) => changeSquareValue(e)}/>
-  ));
-  return (<>{squares}</>);
+  const createInitialBoard = () => {
+    [...Array(9).keys()].map(v => ({id: v, label: "O"}))
+  };
+  const [squares, setSquares] = useState(() => createInitialBoard());
+
+  const changeSquareValue = clickedSquare => {
+    setSquares(squares.map(sqaure => {
+      sqaure.id === clickedSquare.id ? clickedSquare : sqaure
+    }));
+  };
+
+  const X2O = str => str === "X" ? "O" : "X";
+
+  return (
+    <>
+      {squares.map(square => (
+        <Square key={square.id} value={square.label}
+          whenClicked={s => changeSquareValue({...s, label: X2O(e.target.value)})} />
+      ))}
+    </>
+  );
 }
